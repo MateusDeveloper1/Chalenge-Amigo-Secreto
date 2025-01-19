8aece57a2ae55ea3d3cf42eeabfefc899f148ac5
Mateus Silva <mateusilva767@gmail.com>
2025-01-19 15:31:35 -0300

    função para atualizar lista

--- app.js
+++ app.js
@@ -10,4 +10,15 @@ function adicionarAmigo() {
   } else {
     alert("Por favor, insira um nome.");
   }
+
+  listaAmigos();
+}
+
+function listaAmigos() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    lista.innerHTML += `<li>${amigos[i]}</li>`;
+  }
 }
