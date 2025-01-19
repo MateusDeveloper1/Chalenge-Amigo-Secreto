6bf3679d8aca05e3a52bea0d7b7bb5f94ee276fb
Mateus Silva <mateusilva767@gmail.com>
2025-01-19 16:57:43 -0300

    criada a função que sorteia o amigo

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let amigos = [];
+let lista = document.getElementById("listaAmigos");
 
 function adicionarAmigo() {
   let inputAmigo = document.getElementById("amigo");
@@ -15,10 +16,31 @@ function adicionarAmigo() {
 }
 
 function listaAmigos() {
-  let lista = document.getElementById("listaAmigos");
   lista.innerHTML = "";
 
   for (let i = 0; i < amigos.length; i++) {
     lista.innerHTML += `<li>${amigos[i]}</li>`;
+  }
+}
+
+function sortearAmigo() {
+  let tamanhoLista = amigos.length;
+
+  if (amigos.length >= 0) {
+    let amigoAleatorio = Math.floor(Math.random() * tamanhoLista);
+    let amigoSorteado = amigos[amigoAleatorio];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = amigoSorteado;
+
+    const index = amigos.indexOf(amigoSorteado);
+
+    if (index > -1) {
+      amigos.splice(index, 1);
+    } else {
+      alert("Sua lista Acabou");
+      resultado.innerHTML = "";
+      lista.innerHTML = "";
+    }
   }
 }
