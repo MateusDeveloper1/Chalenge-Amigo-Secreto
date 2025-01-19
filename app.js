5984b81afb7c280dff2860f30af674642893eb6b
Mateus Silva <mateusilva767@gmail.com>
2025-01-19 15:30:14 -0300

    função para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 let amigos = [];
+
+function adicionarAmigo() {
+  let inputAmigo = document.getElementById("amigo");
+  let amigoAdicionado = inputAmigo.value;
+
+  if (amigoAdicionado) {
+    amigos.push(amigoAdicionado);
+    inputAmigo.value = "";
+  } else {
+    alert("Por favor, insira um nome.");
+  }
+}
